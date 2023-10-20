6aa8c5d738899762702c3bc64a511d1d030355b3
annierhodell <annie.r.h.odell@gmail.com>
2023-10-20 14:23:07 +0100

    beginning project

---
+++ DataSciencePJ.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+ae_data <- weekly_ae_activity_20231008
+
+ae_data %>%
+  group_by(WeekEndingDate) %>%
+  count()
+
+ae_data %>%
+  group_by(TreatmentLocation) %>%
+  count()
+
+ae_data %>%
+  filter(TreatmentLocation == "G306H")
