a1d7878c54a20b8a77ebdd4ee4c1c56dee0d211f
alicebullard <alice.m.bullard@gmail.com>
2023-10-22 11:19:04 +0100

    full table grouped by HBT

--- DataSciencePJ.R
+++ DataSciencePJ.R
@@ -23,6 +23,15 @@ a_e_data %>%
   group_by(TreatmentLocation) %>%
   summarise(average = mean(NumberOfAttendancesEpisode)) #showing what the average n. of attendance episodes is for each hospital across the whole time period
 
+a_e_data %>%
+  group_by(HBT) %>%
+  summarise(mean_attendances = mean(NumberOfAttendancesEpisode),
+            mean_within_4_hours = mean(NumberWithin4HoursEpisode),
+            mean_over_four_hours = mean(NumberOver4HoursEpisode),
+            mean_over_8_hours = mean(NumberOver8HoursEpisode),
+            mean_over_12_hours = mean(NumberOver12HoursEpisode)) %>%
+  view()# average by HBT not including the percentages
+
 A_E_Data <- left_join(a_e_data, HBT_codes, by ="HBT")
 
     
