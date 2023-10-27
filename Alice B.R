eda47094b9360ad943e2307dc7df2cf51f0517df
alicebullard <alice.m.bullard@gmail.com>
2023-10-27 14:19:37 +0100

    #5

--- Alice B.R
+++ Alice B.R
@@ -1,6 +1,6 @@
 AE_Data %>%
   mutate(Year = year(WeekEndingDate)) %>%
-  mutate(date = )
+  mutate(date = daymonth(WeekEndingDate))
   toString(Year)
 
 
@@ -8,7 +8,7 @@ AE_Data %>%
   select(WeekEndingDate, NumberOfAttendancesEpisode) %>%
   group_by(WeekEndingDate) %>%
   summarise(Sum_NumberOfAttendancesEpisode = sum(NumberOfAttendancesEpisode)) %>%
-  ggplot(aes(x = WeekEndingDate, y = Sum_NumberOfAttendancesEpisode, colour = year(WeekEndingDate))) +
+  ggplot(aes(x = WeekEndingDate, y = Sum_NumberOfAttendancesEpisode, colour = as.factor(year(WeekEndingDate)))) +
   geom_line()
 
 toString(...)
