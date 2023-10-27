34e7c1127e8a0a8ca3a69dd8dc1d26f4df84fcbb
annierhodell <annie.r.h.odell@gmail.com>
2023-10-27 14:37:51 +0100

    clean

--- Annie.R
+++ Annie.R
@@ -4,9 +4,7 @@ tidy.data <- weekly_ae_activity_20231008 %>%
   select(-Country, -DepartmentType) %>%
   mutate(WeekEndingDate = ymd(WeekEndingDate)) %>%
   mutate(PercentageOver4HoursEpisode = 
-           round((NumberOver4HoursEpisode/NumberOfAttendancesEpisode)*100, digits = 1)) %>%
-  relocate(PercentageWithin4HoursEpisode, .before = NumberOver4HoursEpisode) %>%
-  relocate(PercentageOver4HoursEpisode, .before = NumberOver8HoursEpisode)
+           round((NumberOver4HoursEpisode/NumberOfAttendancesEpisode)*100, digits = 1))
 
 view(tidy.data)
 
