ef9e4905a7753826fbee49527fbd83595ac0f5ae
annierhodell <annie.r.h.odell@gmail.com>
2023-10-27 14:45:37 +0100

    tidy

--- Annie.R
+++ Annie.R
@@ -6,22 +6,18 @@ tidy.data <- weekly_ae_activity_20231008 %>%
   mutate(PercentageOver4HoursEpisode = 
            round((NumberOver4HoursEpisode/NumberOfAttendancesEpisode)*100, digits = 1))
 
-view(tidy.data)
-
 data.HBT.pop <- left_join(data, HBT_area_names_and_population, by ="HBT")
-view(data.HBT.pop)
 
 data.TLnames <- Treatement_Location_names%>%
   rename("TreatmentLocation" = "Postcode of Treatment Location")%>%
   left_join(data2, Treatement_Location_names, by ="TreatmentLocation")
-
-view(data.TLnames)
 
 col_order <- c("WeekEndingDate", "HBT", "Area name", "Population", "TreatmentLocation",
                "Name of Treatement Location", "NumberOfAttendancesEpisode", "NumberWithin4HoursEpisode",
                "PercentageWithin4HoursEpisode", "NumberOver4HoursEpisode", "PercentageOver4HoursEpisode", 
                "NumberOver8HoursEpisode", "PercentageOver8HoursEpisode", "NumberOver12HoursEpisode", "PercentageOver12HoursEpisode")
 
-ae_data <- data3[, col_order]
+data.ordered <- data3[, col_order]
+colnames(data.ordered)
 
 view(ae_data)
