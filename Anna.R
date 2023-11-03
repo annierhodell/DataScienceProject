a21c5e9abce1c70cd018044c0529169a353cadf9
annascollay <s2480899@ed.ac.uk>
2023-11-03 14:52:22 +0000

    Tidying up graph 3

--- Anna.R
+++ Anna.R
@@ -1,15 +1,11 @@
-#rename section so we can dicuss what we can rename columns togethers
-#rename("HealthBoardAreaCode" = "HBT", "HealthBoardAreaName" = "Name", 
-#"No.AttendencesEp" = "NumberOfAttendancesEpisode", 
-#"No.within_4hr_Ep" = "NumberWithin4HoursEpisode", "No.Over_4hr_Ep" = "NumberOver4HoursEpisode",
-#"%_within_4hr_Ep" = "PercentageWithin4HoursEpisode", "No.Over_8hr_Ep" = "NumberOver8HoursEpisode",
-#"%_Over_8hr_Ep" = "PercentageOver8HoursEpisode", "No.Over_12hr_Ep" = "NumberOver12HoursEpisode",
-#"%_Over_12hr_Ep" = "PercentageOver12HoursEpisode")
-
-AE_Data %>%
-  select(WeekEndingDate, NumberOfAttendancesEpisode) %>%
-  group_by(WeekEndingDate) %>%
-  summarise(Sum_NumberOfAttendancesEpisode = sum(NumberOfAttendancesEpisode)) %>%
-  filter(year(WeekEndingDate) == 2020) %>%
-  ggplot(aes(x = WeekEndingDate, y = Sum_NumberOfAttendancesEpisode)) +
-  geom_line()
+#Graph 3 - Number of attendees per month in 2020
+data %>%
+  select(Week_Ending_Date, Total_Attendees) %>%
+  group_by(Week_Ending_Date) %>%
+  summarise(Sum_Total_Attendees= sum(Total_Attendees)) %>%
+  filter(year(Week_Ending_Date) == 2020) %>%
+  ggplot(aes(x = Week_Ending_Date, y = Sum_Total_Attendees)) +
+  geom_line()+
+  labs(x = "Date",
+       y = "Sum of Total Attendees",
+       title = "Number of Attendees Per Month In 2020")
