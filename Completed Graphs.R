0b288bcdcffda49d0faf626325f1112d248c0f40
alicemiller04 <a.miller@millerlog.net>
2023-11-04 14:56:59 +0000

    folder tidy

---
+++ Completed Graphs.R
@@ -0,0 +1,57 @@
+#Graph 1
+#graph of yearly total attendees to all hospitals
+#change date label so it is in months "jan feb march..." instead of days
+data_graph_1 <- data %>%
+  mutate(Year = year(Week_Ending_Date)) %>%
+  mutate(date = yday(Week_Ending_Date))
+data_graph_1 %>% 
+  group_by(Week_Ending_Date) %>%
+  summarise(sumdat=sum(Total_Attendees), Year, date) %>% 
+  ggplot(aes(x = date, y = sumdat, colour = as.factor(Year))) +
+  geom_line()+
+  labs(x = "Day of Year",
+       y = "Number of Attendees Across Scotland",
+       title = "Number of A&E Attendees across each year",
+       colour = "Year")
+
+
+
+
+
+
+#graph 2
+#looking at mean attendances in each location per week.
+#we want to add in population data so we get mean attendances as a proportion of population
+# also might want to look at each year individually, or remove 2020,2021 or make there own graph
+#using old dataset needs to be changed to tidy one,
+# do better titles and subtitles
+data_graph_2 <- data %>%
+  group_by(Health_Board_Area_Name) %>%
+  summarise(mean_attendances = mean(Total_Attendees),
+            mean_within_4_hours = mean(Attendees_within_4hrs),
+            mean_over_four_hours = mean(Attendees_Over_4hrs),
+            mean_over_8_hours = mean(Attendees_Over_8hrs),
+            mean_over_12_hours = mean(Attendees_Over_12hrs))
+
+
+# average by HBT not including the percentages
+data_graph_2 %>%
+  ggplot() +
+  geom_col(mapping = aes(x = mean_attendances, y = Health_Board_Area_Name)) +
+  labs(title = "mean attendances per week in each location since 2015")
+# bar plot of the averages grouped by names of the hospital location
+
+
+
+#Graph 3
+#Graph 3 - Number of attendees per month in 2020
+data %>%
+  select(Week_Ending_Date, Total_Attendees) %>%
+  group_by(Week_Ending_Date) %>%
+  summarise(Sum_Total_Attendees= sum(Total_Attendees)) %>%
+  filter(year(Week_Ending_Date) == 2020) %>%
+  ggplot(aes(x = Week_Ending_Date, y = Sum_Total_Attendees)) +
+  geom_line()+
+  labs(x = "Date",
+       y = "Sum of Total Attendees",
+       title = "Number of Attendees Per Month In 2020")
