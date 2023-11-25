690d588decf5e3f1223a88b15409ad36e43cf1b4
annascollay <s2480899@ed.ac.uk>
2023-11-25 13:38:44 +0000

    3

--- Investigation/Anna.R
+++ Investigation/Anna.R
@@ -97,15 +97,17 @@ New_A_E_Data <- a_and_e_hb_agesex_20231005 %>%
   mutate(WeekEnding = ymd(WeekEnding))%>%
   rename("HBT" = HB)
 
-New_AE_data
 
-New_AE_data <- 
+Age_and_Sex_Data <- 
   left_join(New_A_E_Data, HBT_area_names_and_population, by ="HBT")
 
 col_order_NEWDATA <- c("WeekEnding", "HBT", "Area name", "Population", "AgeGroup",
                  "Sex", "NumberAttendances", "Average20182019")
   
-New_AE_data <- New_AE_data[, col_order_NEWDATA]
+Age_and_Sex_Data <- Age_and_Sex_Data[, col_order_NEWDATA]
+
+view(Age_and_Sex_Data)
+
 
 
  
