1886fe1d3825a0523fb24fbbd722c6ce945e354d
alicemiller04 <a.miller@millerlog.net>
2023-11-28 13:58:28 +0000

    added piechart

--- Birds/Bird Graphs.R
+++ Birds/Bird Graphs.R
@@ -1,5 +1,12 @@
 #bird graphs
 
+#colours for trophic levels
+
+trophic_colors <- c("Herbivore" = "darkgreen",
+                    "Carnivore" = "tomato3",
+                    "Omnivore" = "steelblue",
+                    "Scavenger" = "wheat")
+
 #bar chart of trophic level
 
 birds_data%>%
@@ -9,6 +16,22 @@ birds_data%>%
   labs(title = "Distribution of Trophic Levels",
        x = "Trophic Level",
        y = "Count")
+
+#proportion pie chart of different trophic levels
+
+birds_data %>%
+  filter(!is.na(General_Trophic)) %>%
+  ggplot(aes(x = "", fill = General_Trophic)) +
+  geom_bar(width = 1, stat = "count") +
+  coord_polar(theta = "y") +
+  scale_fill_manual(values = trophic_colors, name = "Trophic Type") +  # Assign colors and change legend name
+  guides(fill = guide_legend(title = "Trophic Type")) +  # Change legend name
+  labs(title = "Distribution of Trophic Types") +
+  theme(axis.text = element_blank(),  # Remove axis text
+        axis.title = element_blank(),  # Remove axis title
+        legend.position = "bottom")  # Move legend to the bottom
+
+# pie chart for 
 
 
 #another bar chart this time showing habitat
